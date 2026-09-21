a44351c9e5fcdc108e7abf468368c6b68b7d3b8c
crazydog <ax02tom2@gmail.com>
2026-09-21 14:12:37 +0800

    Update ebook_downloader.py

--- ebook_downloader.py
+++ ebook_downloader.py
@@ -99,7 +99,7 @@ with tab2:
                         "q": refined_query,
                         "gl": "tw",
                         "hl": "zh-tw",
-                        "num": 100
+                        "num": 50
                     })
                     headers = {
                         'X-API-KEY': clean_key,
